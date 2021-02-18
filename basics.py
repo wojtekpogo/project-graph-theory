9937af7faca3213ba9f43836441665acc2ef654e
wojtekpogo <w.pogorzelski95@gmail.com>
2021-02-18 21:46:50 +0000

    Basics of python

---
+++ basics.py
@@ -0,0 +1,15 @@
+# Wojciech Pogorzelski 
+#Python Basics
+#2021-02-18
+
+print("Hello World!")
+
+a = 1
+b = 1.5
+
+s = "String"
+t='String in the single quote'
+
+print(a,b,s,t)
+
+print(t[3:10:2]) #String slice, works like a for loop, starts from character at index 2 and displays every second one
