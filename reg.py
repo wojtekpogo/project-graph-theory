6ee3e176825e3ccabcfdde026f82a4f0e0e3b89a
wojtekpogo <w.pogorzelski95@gmail.com>
2021-04-29 01:10:41 +0100

    Added arg Parser

--- reg.py
+++ reg.py
@@ -1,26 +1,56 @@
 import shuntingre
 import thompson
+from argparse import ArgumentParser, RawTextHelpFormatter #import argparser
+import sys #to take arguments
 
-#with open('file.txt', 'r') as file:
-
-    # Read a line of file
-    # Check if it matches the regular expression
+# a*: zero or more a's.
+# a+: one or more a's. i.e. aa*
 
 
-if __name__ == "__main__":
-    tests = [  ["(a.b|b*)",   ["ab", "b", "bb", "a"]]
-             , ["a.(b.b)*.a", ["aa", "abba", "aba"]]
-             , ["1.(0.0)*.1", ["11", "100001", "11001"]]
-    ]
+def get_arg_parser():
+    argparser = ArgumentParser(formatter_class=RawTextHelpFormatter)
 
-    for test in tests:
-        infix = test[0]
-        print(f"infix:    {infix}")
+    argparser.add_argument('--regex', dest='regex',
+                           action='append',
+                           help='Regex that will be run against a text file')
+    argparser.add_argument('--file', dest='file',
+                           action='append',
+                           help='Full path to a text file')
+
+    return argparser
+
+# Get the arguments that are passed in while running the script
+def get_main_args(sys_argv):
+    arg_parser = get_arg_parser()
+    if len(sys_argv) <= 1: #If no arguments passed print help message only
+        arg_parser.print_help() 
+        raise SystemExit(2)
+    return get_arg_parser().parse_args(sys_argv[1:]) # Return all passed arguments
+
+def main(args):
+    _args = get_main_args(args)
+    if _args.regex:
+        if _args.file:
+            print("\nLooking for \'{0}\' regular expression matches in {1}\n".format(_args.regex[0], _args.file[0]))
+            regex(_args.file[0], _args.regex[0])
+        else:
+            print("No file specified")
+    else:
+        print("No regex specified")
+
+
+# Function takes the text file path, and regex, and then searches for matches of the pattern
+def regex(text_file, regex_match):
+    with open(text_file, 'r') as the_file:
+        text = the_file.readlines()
+        infix = regex_match
         postfix = shuntingre.shunt(infix)
-        print(f"postfix:  {postfix}")
         nfa = thompson.re_to_nfa(postfix)
-        print(f"thompson: {nfa}")
-        for s in test[1]:
-            match = nfa.match(s)
-            print(f"Match '{s}': {match}")
-        print()
+        for word in text:
+            word = word.strip()
+            match = nfa.match(word)
+            print("Match {0}: {1}".format(word, match)) # Prints found matches
+
+
+if __name__ == '__main__':
+    main(sys.argv)
